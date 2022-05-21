ddb03778f975909df1014fbea14d27b88855ea3a
Nihan Zyan <nihanzyan38@gmail.com>
2022-05-21 22:50:49 +0600

    Custom Theme Added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#a2b3f9",
+          "secondary": "#188196",
+          "accent": "#f9c85c",
+          "neutral": "#151A1E",
+          "base-100": "#EDE7F3",
+          "info": "#9BB7F3",
+          "success": "#28B888",
+          "warning": "#FAB952",
+          "error": "#ED3557",
+        },
+      },
+    ],
   },
   plugins: [require("daisyui")],
 }
